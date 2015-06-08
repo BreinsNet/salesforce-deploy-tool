d614fa29a8645eb4858f8ddbbcf57dcb5b656edb
Juan Breinlinger <juan.brein@breins.net>
2015-06-08 12:12:24 +0100

    bug fix when checking -v, improved mock ant

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,13 +2,6 @@ require 'aruba/cucumber'
 require 'filewatcher'
 require 'git'
 
-# Necesary environment variables:
-ENV["SFDT_GIT_REPO"] = File.join(Dir.pwd,'features/resources/repo')
-ENV["SFDT_GIT_DIR"] = 'repo'
-ENV["SFDT_SRC_DIR"] = 'salesforce/src'
-ENV["SFDT_USERNAME"] = 'john.doe@example.com'
-ENV["SFDT_PASSWORD"] = 'mysecurepass'
-ENV["SFDT_SANDBOX"] = 'testEnv'
 
 # Create a temprary home directory:
 new_home = File.join(Dir.pwd,'tmp','home') 
@@ -28,11 +21,21 @@ Before '@config' do
   FileUtils.rm_rf File.join(File.expand_path('~/'),'.sf')
 end
 
-# Before push and pull clone the repository
-Before '@push,@pull' do 
+Before '@push,@pull,@config' do 
+
+  # Necesary environment variables:
+  ENV["SFDT_GIT_REPO"] = File.join(Dir.pwd,'features/resources/repo')
+  ENV["SFDT_GIT_DIR"] = 'repo'
+  ENV["SFDT_SRC_DIR"] = 'salesforce/src'
+  ENV["SFDT_USERNAME"] = 'john.doe@example.com'
+  ENV["SFDT_PASSWORD"] = 'mysecurepass'
+  ENV["SFDT_SANDBOX"] = 'testEnv'
+
+  # Clone repository
   uri = ENV['SFDT_GIT_REPO']
   name = File.join 'tmp', 'aruba', ENV['SFDT_GIT_DIR']
   Git.clone(uri, name)
+
 end
 
 at_exit do
